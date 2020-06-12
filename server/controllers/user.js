339d569d0642607f66fcc90f34f87a55b6a16fc3
Muh Ichsan Kamil <muhichsank@gmail.com>
2020-06-12 11:51:46 +0800

    sama server

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,49 @@
 const { User } = require('../models')
 const Helper = require('../helpers/helper.js')
+const { OAuth2Client } = require('google-auth-library');
 
 class Controller{
+    static googleSign(req, res, next) {
+        let { id_token } = req.body;
+        let email;
+        const client = new OAuth2Client(process.env.CLIENT_ID);
+        client.verifyIdToken({
+            idToken: id_token,
+            audience: process.env.CLIENT_ID
+        })
+            .then(ticket => {
+                email = ticket.getPayload().email;
+
+                return User.findOne({
+                    where: { email }
+                })
+            })
+            .then(data => {
+                if (data) {
+                    return {
+                        id: data.id,
+                        email: data.email
+                    }
+                } else {
+                    return User.create({ email, password: 'admin123' })
+                }
+            })
+            .then(data => {
+                let payload = {
+                    id: data.id,
+                    email: data.email
+                }
+                return res.status(201).json({
+                    data: {
+                        id: data.id,
+                        email: data.email,
+                        token: Helper.generateToken(payload)
+                    }
+                })
+            })
+            .catch(next)
+    }
+
     static register(req, res, next){
         User.create({
             email : req.body.email,
