58e201c7218a1be3e2629ecb2dcaf4f15d040240
Muh Ichsan Kamil <muhichsank@gmail.com>
2020-06-12 10:49:54 +0800

    OAuth

--- client/main.js
+++ client/main.js
@@ -9,15 +9,41 @@ function login(event) {
     let email = $('#email').val()
     let password = $('#password').val()
     console.log(email, password, '<<<<<< tes');
+
     $.ajax({
         method: 'post',
-        url: baseUrl + '/login',
+        url: baseUrl + '/users/login',
         data: { email, password }
     })
-        .done(({data}) => {
+        .done((data) => {
+            // console.log(data, '<<<<<<< Hasil console');
             localStorage.setItem('token', data.token);
         })
         .fail(err => {
             console.log(err, '<<<<<< err');
         })
+}
+
+function onSignIn(googleUser) {
+    let id_token = googleUser.getAuthResponse().id_token;
+    $.ajax({
+        method: 'post',
+        url: baseUrl + '/users/googleSign',
+        data: { id_token }
+    })
+        .done(data => {
+            console.log(data, '<<<<< data signIn');
+            localStorage.setItem('token', data.token)
+        })
+        .fail(err => {
+            console.log(err.response.JSON.errors, '<<<< err signIn');
+        })
+}
+
+function signOut() {
+    localStorage.clear()
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
 }
