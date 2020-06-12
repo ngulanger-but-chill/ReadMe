367e80cf2972270ad1bd155bd2fd937a8abacd9c
Sulaksana16 <sulaksana16@gmail.com>
2020-06-12 16:04:15 +0700

    jquery show hide

--- client/main.js
+++ client/main.js
@@ -1,8 +1,52 @@
 let baseUrl = 'http://localhost:3000'
 
+if (localStorage.token) {
+    $('.container').show()
+    $('.login-page').hide()
+    $('.register-page').hide()
+} else {
+    $('.login-page').show()
+    $('.register-page').hide()
+    $('.container').hide()
+}
+
 $(document).ready(function () {
-    console.log('masuk');
+    // console.log('masuk');
+    $('.login-page #newuser').on("click", (event) => {
+        event.preventDefault()
+        login(event)
+        $('.login-page').hide();
+        $('.register-page').show()
+    });
+
+    $('.register-page').on("submit", (event) => {
+        event.preventDefault()
+        let email = $('#emailRegister').val()
+        let password = $('#passwordRegister').val()
+        // console.log(email, password, '<<<<<< tes');
     
+        $.ajax({
+            method: 'post',
+            url: baseUrl + '/users/register',
+            data: { email, password }
+        })
+            .done((data) => {
+                $('#register-status').text('Successfully register ^^')
+            })
+            .fail(err => {
+                $('#register-status').text(err.responseJSON.message)
+            })
+            .always(() => {
+                $('#email').val('')
+                $('#password').val('')
+            })
+    })
+
+    $('.register-page #login-newuser').on("click", (event) => {
+        event.preventDefault()
+        $('.login-page').show();
+        $('.register-page').hide()
+    });
 });
 
 function login(event) {
@@ -29,28 +73,6 @@ function login(event) {
         })
 }
 
-function register(event) {
-    event.preventDefault()
-    let email = $('#emailRegister').val()
-    let password = $('#passwordRegister').val()
-    console.log(email, password, '<<<<<< tes');
-
-    $.ajax({
-        method: 'post',
-        url: baseUrl + '/users/register',
-        data: { email, password }
-    })
-        .done((data) => {
-            $('#register-status').text('Successfully register ^^')
-        })
-        .fail(err => {
-            $('#register-status').text(err.responseJSON.message)
-        })
-        .always(() => {
-            $('#email').val('')
-            $('#password').val('')
-        })
-}
 
 function onSignIn(googleUser) {
     let id_token = googleUser.getAuthResponse().id_token;
